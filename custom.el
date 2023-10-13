72d09fa65e0f740c6893100324d68d45045758e2
Jon Boone <ipmonger@delamancha.org>
2023-10-13 00:18:47 -0400

    add crafted-completion-packages module support

--- custom.el
+++ custom.el
@@ -3,8 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(completion-category-overrides '((file (styles partial-completion))))
+ '(completion-styles '(orderless basic))
+ '(corfu-auto t)
+ '(corfu-auto-prefix 2)
+ '(corfu-cycle t)
  '(load-prefer-newer t t)
- '(mouse-wheel-progressive-speed nil))
+ '(marginalia-annotators
+   '(marginalia-annotators-heavy marginalia-annotators-light nil) t)
+ '(mouse-wheel-progressive-speed nil)
+ '(vertico-cycle t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -17,3 +17,13 @@
 ;; For now, we will begin by using crafted-init-config module
 (load (expand-file-name "modules/crafted-init-config"
 			crafted-emacs-home))
+
+;; add package definitions for completion packages
+;; to 'package-selected-packages'.
+(require 'crafted-completion-packages)
+
+;; install selected packages
+(package-install-selected-packages :noconfirm)
+
+;; load configuration for the completion module
+(require 'crafted-completion-config)
