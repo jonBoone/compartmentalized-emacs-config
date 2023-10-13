97a9c37a3d67dd2be39f0e1a27fd6faa727d8042
Jon Boone <ipmonger@delamancha.org>
2023-10-13 00:07:19 -0400

    adding custom.el

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(load-prefer-newer t t)
+ '(mouse-wheel-progressive-speed nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
