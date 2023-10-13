4c501d30e7db693e2120c304f3d72e7f5bb95749
Jon Boone <ipmonger@delamancha.org>
2023-10-13 00:25:21 -0400

    adds default settings customizations

--- custom.el
+++ custom.el
@@ -3,16 +3,36 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method 'aggressive t)
+ '(bookmark-save-flag 1 t)
  '(completion-category-overrides '((file (styles partial-completion))))
+ '(completion-cycle-threshold 3)
  '(completion-styles '(orderless basic))
+ '(completions-detailed t)
  '(corfu-auto t)
  '(corfu-auto-prefix 2)
  '(corfu-cycle t)
+ '(dired-auto-revert-buffer t t)
+ '(dired-dwim-target t t)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain t)
+ '(eshell-scroll-to-bottom-on-input 'this t)
+ '(fast-but-imprecise-scrolling t)
+ '(global-auto-revert-non-file-buffers t)
+ '(ibuffer-movement-cycle nil t)
+ '(ibuffer-old-time 24 t)
+ '(kill-do-not-save-duplicates t)
  '(load-prefer-newer t t)
  '(marginalia-annotators
    '(marginalia-annotators-heavy marginalia-annotators-light nil) t)
  '(mouse-wheel-progressive-speed nil)
- '(vertico-cycle t))
+ '(scroll-conservatively 101)
+ '(scroll-margin 0)
+ '(scroll-preserve-screen-position t)
+ '(switch-to-buffer-in-dedicated-window 'pop)
+ '(switch-to-buffer-obey-display-actions t)
+ '(tab-always-indent 'complete)
+ '(vertico-cycle t)
+ '(xref-show-definitions-function 'xref-show-definitions-completing-read t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
