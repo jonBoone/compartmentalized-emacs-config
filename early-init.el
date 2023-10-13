eaa87c60d194531374bd5a5d544d3d3774ea0368
Jon Boone <ipmonger@delamancha.org>
2023-10-12 23:52:22 -0400

    update additional parameters that could interfere with straight.el

--- early-init.el
+++ early-init.el
@@ -14,11 +14,22 @@
 ;; minimize gc runs
 (setq gc-cons-threshold (* 200 1024 1024))
 
+;; disable package.el in favor of straight.el
+(setq package-enable-at-startup nil)
+
+;; don't process file-handlers yet
+(defvar file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;; don't search for init.el in site-lisp
+(setq site-run-file nil)
+
 ;; remove unnecessary UI elements
 (unless (and (display-graphic-p) (eq system-type 'darwin))
   (push '(menu-bar-lines . 0) default-frame-alist))
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . 0) default-frame-alist)
+
 
 (setq crafted-emacs-home "~/.config/emacs/crafted-emacs")
 (load (expand-file-name "custom-modules/crafted-early-init-straight"
