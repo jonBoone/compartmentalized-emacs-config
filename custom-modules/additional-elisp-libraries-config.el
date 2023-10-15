6dfcbc41f14f836825c20bea67a1fbde6025498c
Jon Boone <ipmonger@delamancha.org>
2023-10-15 18:21:40 -0400

    add additional-elisp-libraries configuration

---
+++ custom-modules/additional-elisp-libraries-config.el
@@ -0,0 +1,29 @@
+;;; additional-elisp-libraries-config.el --- additional elisp libraries config  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone
+
+;;; Commentary:
+
+(use-package comment-tags
+  :straight t)
+
+(use-package dash
+  :straight t)
+
+(use-package ht
+  :straight t)
+
+(use-package s
+  :straight t)
+
+(use-package ts
+  :straight t)
+
+(provide 'additional-elisp-libraries-config)
+;; END additional-elisp-libraries-config.el
+;;
+
+;; additional-elisp-libraries-config.el ends here
