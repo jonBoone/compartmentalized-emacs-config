e380516411c1f6ec054c57082e4cc96c578d9b06
Jon Boone <ipmonger@delamancha.org>
2023-10-16 10:34:26 -0400

    correct typo in 'provide argument

---
+++ custom-modules/initial-buffers-config.el
@@ -0,0 +1,17 @@
+;;; initial-buffers-config.el --- initial buffers config  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone
+
+;;; Commentary:
+
+;; don't show the startup message
+(setq inhibit-startup-message t)
+
+(provide 'initial-buffers-config)
+;; END initial-buffers-config.el
+;;
+
+;; initial-buffers-config.el ends here
