ba768865df5913a946c2dd8b60b853faead9c475
Jon Boone <ipmonger@delamancha.org>
2023-10-16 19:08:01 -0400

    add dired support

--- custom-modules/my-files-packages.el
+++ custom-modules/my-files-packages.el
@@ -10,6 +10,11 @@
 ;;; Code:
 
 (add-to-list 'package-selected-packages 'super-save)
+(add-to-list 'package-selected-packages 'nerd-icons-dired)
+(add-to-list 'package-selected-packages 'dired)
+(add-to-list 'package-selected-packages 'dired-rainbow)
+(add-to-list 'package-selected-packages 'dired-single)
+(add-to-list 'package-selected-packages 'dired-x)
 
 (provide 'my-files-packages)
 ;; END my-files-packages.el
