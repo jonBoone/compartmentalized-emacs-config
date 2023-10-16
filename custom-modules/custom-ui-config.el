d930be067f4898d0ee158aab0570d5f6010b3a1d
Jon Boone <ipmonger@delamancha.org>
2023-10-16 11:55:14 -0400

    update custom-ui-config with additional parameters

--- custom-modules/custom-ui-config.el
+++ custom-modules/custom-ui-config.el
@@ -7,6 +7,62 @@
 
 ;;; Commentary:
 
+
+;; customize truncation indicators and add breathing room at the edge of the window
+(define-fringe-bitmap 'right-curly-arrow
+  [#b10000000
+   #b10000000
+   #b01000000
+   #b01000000
+   #b00100000
+   #b00100000
+   #b00010000
+   #b00010000
+   #b00001000
+   #b00001000
+   #b00000100
+   #b00000100])
+(define-fringe-bitmap 'left-curly-arrow
+  [#b00000100
+   #b00000100
+   #b00001000
+   #b00001000
+   #b00010000
+   #b00010000
+   #b00100000
+   #b00100000
+   #b01000000
+   #b01000000
+   #b10000000
+   #b10000000])
+
+(set-fringe-mode 10)
+
+;; use visual bell
+(setq visible-bell t)
+
+;; narrow the gap betwen emacs frames and other os windows
+(setq frame-resize-pixelwise t)
+
+;; don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+
+;; turn on column numbers in addition to line numbers
+(column-number-mode)
+
+;; enable line numbers by default
+(global-display-line-numbers-mode t)
+
+;; don't display line numbers for these modes
+(dolist (mode '(comint-mode-hook treemacs-mode-hook))
+  (add-hook mode 'crafted-ui--disable-line-numbers-mode))
+
+;; set the frame transparency
+(add-to-list 'default-frame-alist `(alpha . ,my/frame-transparency))
+
+;; ensure that block cursor when using xlib is the same size as the glyph
+(setq x-stretch-cursor t)
+
 ;; manage popup windows with popper
 (use-package popper
   :straight t
