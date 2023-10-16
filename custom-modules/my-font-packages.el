de2f98f5f3dd67f26c3d2309069b9b20ee776c0e
Jon Boone <ipmonger@delamancha.org>
2023-10-16 13:00:56 -0400

    add missing my-font-packages file

---
+++ custom-modules/my-font-packages.el
@@ -0,0 +1,19 @@
+;;; my-font-packages.el --- add-on elisp libraries module  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone
+
+;;; Commentary:
+
+;;; Code:
+
+;; use nerd-icons
+(add-to-list 'package-selected-packages 'nerd-icons)
+
+(provide 'my-font-packages)
+;; END my-font-packages.el
+;;
+
+;; my-font-packages.el ends here
