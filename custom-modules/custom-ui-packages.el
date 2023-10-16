640df8d31b1fec49235ae25daaf7766780ba44d5
Jon Boone <ipmonger@delamancha.org>
2023-10-16 12:51:22 -0400

    add settings from crafted-ui module to custom-ui module

--- custom-modules/custom-ui-packages.el
+++ custom-modules/custom-ui-packages.el
@@ -8,7 +8,15 @@
 ;;; Commentary:
 
 ;;; Code
+(add-to-list 'package-selected-packages 'doom-modeline)
+(add-to-list 'package-selected-packages 'elisp-demos)
+(add-to-list 'package-selected-packages 'helpful)
+(add-to-list 'package-selected-packages 'minions)
+(add-to-list 'package-selected-packages 'nerd-icons-completion)
+(add-to-list 'package-selected-packages 'nerd-icons-ibuffer)
 (add-to-list 'package-selected-packages 'popper)
+(add-to-list 'package-selected-packages 'smart-mode-line)
+
 
 (provide 'custom-ui-packages)
 ;; END custom-ui-packages.el
