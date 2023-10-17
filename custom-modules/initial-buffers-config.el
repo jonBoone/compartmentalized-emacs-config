99b55fb0849230a8f8a0895610703d5db5ed1221
Jon Boone <ipmonger@delamancha.org>
2023-10-16 20:26:35 -0400

    adding initial-buffers module

--- custom-modules/initial-buffers-config.el
+++ custom-modules/initial-buffers-config.el
@@ -10,6 +10,18 @@
 ;; don't show the startup message
 (setq inhibit-startup-message t)
 
+;; show a dashboard upon start
+(use-package dashboard
+  :straight t
+  :config
+  (setq dashboard-banner-logo-title "Iain's Dashboard"
+        dashboard-startup-banner    (concat user-emacs-directory "Emacs-Is-Sexy-V2-64x64.png")
+        dashboard-center-content    t
+        dashboard-items             '((agenda . 16))
+        dashboard-icon-type         'nerd-icons
+        dashboard-display-icons-p   t)
+        (dashboard-setup-startup-hook))
+
 (provide 'initial-buffers-config)
 ;; END initial-buffers-config.el
 ;;
---
+++ custom-modules/initial-buffers-packages.el
@@ -0,0 +1,18 @@
+;;; initial-buffers-packages.el --- file handling packages -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone
+
+;;; Commentary:
+
+;;; Code:
+
+(add-to-list 'package-selected-packages 'dashboard)
+
+(provide 'initial-buffers-packages)
+;; END initial-buffers-packages.el
+;;
+
+;; initial-buffers-packages.el ends here
