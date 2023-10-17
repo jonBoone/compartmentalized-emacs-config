d1f87314fdeb2fee793369f46a6058f0f6219fee
Jon Boone <ipmonger@delamancha.org>
2023-10-16 20:44:32 -0400

    add custom-writing module

---
+++ custom-modules/custom-writing-config.el
@@ -0,0 +1,27 @@
+;;; custom-writing-config.el --- custom writing configuration  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone
+
+;;; Commentary:
+
+;; turn on global whitespace instead of tabs
+(with-eval-after-load 'crafted-writing-config
+  (crafted-writing-configure-whitespace nil t))
+
+;; set the default tab-width to 2 spaces
+(custom-set-default tab-width 2)
+
+;; automatically cleanup whitespace
+(use-package ws-butler
+  :straight t
+  :hook (prog-mode text-mode))
+
+
+(provide 'custom-writing-config)
+;; END custom-writing-config.el
+;;
+
+;; custom-writing-config.el ends here
---
+++ custom-modules/custom-writing-packages.el
@@ -0,0 +1,18 @@
+;;; custom-ui-packages.el --- supplemental ui packages -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone <ipmonger@delamancha.org>
+
+;;; Commentary:
+
+;;; Code
+(add-to-list 'package-selected-packages 'ws-butler)
+
+
+(provide 'custom-writing-packages)
+;; END custom-writing-packages.el
+;;
+
+;; custom-writing-packages.el ends here
