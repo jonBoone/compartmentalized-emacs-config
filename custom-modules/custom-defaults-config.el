25b871510e0ec0fdd844b57822dddba3be381fbd
Jon Boone <ipmonger@delamancha.org>
2023-10-18 11:30:32 -0400

    add custom-defaults module

--- custom-modules/custom-defaults-config.el
+++ custom-modules/custom-defaults-config.el
@@ -21,9 +21,24 @@
 ;; remember the last cursor location of opened files
 (save-place-mode 1)
 
+;; leverage flyspell package
+(use-package flyspell
+  :straight t
+  :hook (text-mode . flyspell)
+  :diminish
+  :init
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode)
+  :config
+  (setq ispell-program-name "aspell"
+        ispell-extra-args  ("--sug-mode=ultra")))
+
+(use-package flycheck
+  :straight t
+  :hook (after-init . global-flycheck)
+  :diminish)
+
 (provide 'my-defaults-config)
 ;; END my-defaults-config.el
 ;;
 
 ;; my-defaults-config.el ends here
-
---
+++ custom-modules/custom-defaults-packages.el
@@ -0,0 +1,19 @@
+;;; custom-defaults-packages.el --- custom default packages -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone <ipmonger@delamancha.org>
+
+;;; Commentary:
+
+;;; Code
+(add-to-list 'package-selected-packages 'flyspell)
+(add-to-list 'package-selected-packages 'flycheck)
+
+
+(provide 'custom-defaults-packages)
+;; END custom-defaults-packages.el
+;;
+
+;; custom-defaults-packages.el ends here
