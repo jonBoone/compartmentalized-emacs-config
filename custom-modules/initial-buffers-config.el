aad288235f2c235f38673bac8e4f300622d28dd0
Jon Boone <ipmonger@delamancha.org>
2023-11-27 18:15:14 -0500

    update dashboard footer settings

--- custom-modules/initial-buffers-config.el
+++ custom-modules/initial-buffers-config.el
@@ -17,14 +17,16 @@
 (use-package dashboard
   :straight t
   :init
+
   (setq dashboard-banner-logo-title "Iain's Dashboard"
         dashboard-center-content    t
         dashboard-display-icons-p   t
+        dashboard-footer-icon       (nerd-icons-octicon "nf-oct-workflow")
+        dashboard-footer-messages   '("GTD Steps: 1. Capture  2. Clarify  3. Organize  4. Reflect  5. Engage")
         dashboard-items             '((agenda . 16) (projects . 5))
         dashboard-icon-type         'nerd-icons
         dashboard-projects-backend  'project-el
         dashboard-set-file-icons    t
-        dashboard-set-footer        nil
         dashboard-set-heading-icons t
         dashboard-set-init-info     t
         dashboard-set-navigator     t
