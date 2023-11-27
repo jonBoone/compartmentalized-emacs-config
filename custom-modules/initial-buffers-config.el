396428d5cd5f357904d16dc5cbb874a58023c2f7
Jon Boone <ipmonger@delamancha.org>
2023-11-27 12:46:15 -0500

    update dashboard settings

--- custom-modules/initial-buffers-config.el
+++ custom-modules/initial-buffers-config.el
@@ -16,13 +16,21 @@
 ;; show a dashboard upon start
 (use-package dashboard
   :straight t
-  :config
+  :init
   (setq dashboard-banner-logo-title "Iain's Dashboard"
-        dashboard-startup-banner    (concat user-emacs-directory "Emacs-Is-Sexy-V2-64x64.png")
         dashboard-center-content    t
-        dashboard-items             '((agenda . 16))
+        dashboard-display-icons-p   t
+        dashboard-items             '((agenda . 16) (projects . 5))
         dashboard-icon-type         'nerd-icons
-        dashboard-display-icons-p   t)
+        dashboard-projects-backend  'project-el
+        dashboard-set-file-icons    t
+        dashboard-set-footer        nil
+        dashboard-set-heading-icons t
+        dashboard-set-init-info     t
+        dashboard-set-navigator     t
+        dashboard-startup-banner    (concat user-emacs-directory "Emacs-Is-Sexy-V2-64x64.png")
+        dashboard-week-agenda       t)
+  :config
   (dashboard-setup-startup-hook))
 
 (provide 'initial-buffers-config)
