ebe400afc817f5f0ef76e5a3789f4b5fc1b18d1b
Jon Boone <ipmonger@delamancha.org>
2023-12-02 19:27:46 -0500

    add xwidget-webkit-package support

---
+++ custom-modules/custom-browser-config.el
@@ -0,0 +1,28 @@
+;;; custom-browser-config.el --- custom defaults    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone
+
+;;; Commentary:
+
+
+(use-package xwwp-full
+  :bind
+  (:map xwidget-webkit-mode-map
+        ("<localleader>b" . 'xwidget-webkit-back)
+        ("<localleader>j" . 'xwwp-ace-toggle)
+        ("<localleader>o" . 'xwwp-section)
+        ("<localleader>h" . 'xwwp-history-show))
+  :init
+  (customize-set-value xwwp-follow-link-completion-system 'default)
+  :config
+  (require 'cl-lib))
+
+
+(provide 'custom-browser-config)
+;; END custom-browser-config.el
+;;
+
+;; custom-browser-config.el ends here
---
+++ custom-modules/custom-browser-packages.el
@@ -0,0 +1,17 @@
+;;; custom-browser-packages.el --- custom default packages -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023
+;; SPDX-License-Identifier: MIT
+
+;; Author: Jon Boone <ipmonger@delamancha.org>
+
+;;; Commentary:
+
+;;; Code
+(add-to-list 'package-selected-packages 'xwwp-full)
+
+(provide 'custom-browser-packages)
+;; END custom-browser-packages.el
+;;
+
+;; custom-browser-packages.el ends here
