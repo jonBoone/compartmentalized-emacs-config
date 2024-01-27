a5a30da3ad9f8a8481456c03d554c0543144591f
Jon Boone <ipmonger@delamancha.org>
2024-01-26 22:27:00 -0500

    update custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(Man-notify-method 'aggressive t)
- '(bookmark-save-flag 1)
+ '(bookmark-save-flag 1 t)
  '(completion-category-overrides '((file (styles partial-completion))))
  '(completion-cycle-threshold 3)
  '(completion-styles '(orderless basic))
@@ -13,18 +13,17 @@
  '(corfu-auto-prefix 2)
  '(corfu-cycle t)
  '(custom-safe-themes
-   '("2b501400e19b1dd09d8b3708cefcb5227fda580754051a24e8abf3aff0601f87"
-     default))
+   '("2b501400e19b1dd09d8b3708cefcb5227fda580754051a24e8abf3aff0601f87" default))
  '(dired-auto-revert-buffer t)
  '(dired-dwim-target t)
- '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain t)
  '(eglot-autoshutdown t)
- '(epg-gpg-program "/opt/homebrew/bin/gpg")
+ '(epg-gpg-program "/opt/homebrew/bin/gpg" t)
  '(eshell-scroll-to-bottom-on-input 'this t)
  '(fast-but-imprecise-scrolling t)
  '(global-auto-revert-non-file-buffers t)
- '(ibuffer-movement-cycle nil)
- '(ibuffer-old-time 24)
+ '(ibuffer-movement-cycle nil t)
+ '(ibuffer-old-time 24 t)
  '(inferior-lisp-program "sbcl")
  '(kill-do-not-save-duplicates t)
  '(load-prefer-newer t t)
@@ -34,26 +33,12 @@
  '(numpydoc-insert-examples-block nil)
  '(numpydoc-template-long nil)
  '(org-agenda-files
-   '("/home/iain/Dropbox/pkb/org/main/2019_atx_midtower_build.org"
-     "/home/iain/Dropbox/pkb/org/main/2023_miniatx_midtower_build.org"
-     "/home/iain/Dropbox/pkb/org/main/security_skill_sets_to_add.org"
-     "/home/iain/Dropbox/pkb/org/main/sliding_window.org"
-     "/home/iain/Dropbox/pkb/org/projects/home-lab/network/2024-01-08-design.org"
-     "/home/iain/Dropbox/pkb/org/projects/home-lab/proxmox/2023-07-04-hardware-configuration.org"
-     "/home/iain/Dropbox/pkb/org/projects/home-lab/proxmox/worklog.org"
-     "/home/iain/Dropbox/pkb/org/projects/open-source/common-lisp/cmucl/worklog.org"
-     "/home/iain/Dropbox/pkb/org/projects/org-mode/worklog.org"
-     "/home/iain/Dropbox/pkb/org/2023-07-25-17-00-Take-Home-Assignment-Follow-Up-with-Robert-Smith.org"
-     "/home/iain/Dropbox/pkb/org/active.org"
-     "/home/iain/Dropbox/pkb/org/future.org"
-     "/home/iain/Dropbox/pkb/org/gtd.org"
-     "/home/iain/Dropbox/pkb/org/inbox.org"
-     "/home/iain/Dropbox/pkb/org/projects.org"))
- '(org-directory "/home/iain/Dropbox/pkb/org/")
- '(org-hide-emphasis-markers t)
- '(org-link-descriptive t)
- '(org-mouse-1-follows-link t)
- '(org-return-follows-link t)
+   '("/Users/iain/Dropbox/pkb/org/main/2019_atx_midtower_build.org" "/Users/iain/Dropbox/pkb/org/main/2023_miniatx_midtower_build.org" "/Users/iain/Dropbox/pkb/org/main/security_skill_sets_to_add.org" "/Users/iain/Dropbox/pkb/org/projects/home-lab/network/2024-01-08-design.org" "/Users/iain/Dropbox/pkb/org/projects/home-lab/proxmox/2023-07-04-hardware-configuration.org" "/Users/iain/Dropbox/pkb/org/projects/home-lab/proxmox/worklog.org" "/Users/iain/Dropbox/pkb/org/projects/open-source/common-lisp/cmucl/worklog.org" "/Users/iain/Dropbox/pkb/org/projects/org-mode/worklog.org" "/Users/iain/Dropbox/pkb/org/2023-07-25-17-00-Take-Home-Assignment-Follow-Up-with-Robert-Smith.org" "/Users/iain/Dropbox/pkb/org/active.org" "/Users/iain/Dropbox/pkb/org/future.org" "/Users/iain/Dropbox/pkb/org/gtd.org" "/Users/iain/Dropbox/pkb/org/inbox.org" "/Users/iain/Dropbox/pkb/org/projects.org") t)
+ '(org-directory "/Users/iain/Dropbox/pkb/org/" t)
+ '(org-hide-emphasis-markers t t)
+ '(org-link-descriptive t t)
+ '(org-mouse-1-follows-link t t)
+ '(org-return-follows-link t t)
  '(package-archive-priorities '(("gnu" . 90) ("nongnu" . 80) ("melpa" . 70)))
  '(python-indent-guess-indent-offset-verbose nil)
  '(scheme-program-name "guile" t)
@@ -65,14 +50,14 @@
  '(switch-to-buffer-obey-display-actions t)
  '(tab-always-indent 'complete)
  '(tabspaces-include-buffers '("*scratch*"))
- '(tabspaces-mode t)
+ '(tabspaces-mode t t)
  '(tabspaces-remove-to-default t)
  '(tabspaces-use-filtered-buffers-as-default t)
  '(vertico-cycle t)
  '(warning-suppress-types '((use-package)))
  '(whitespace-action '(cleanup auto-cleanup))
  '(whitespace-style '(face empty trailing tab-mark indentation::space))
- '(xref-show-definitions-function 'xref-show-definitions-completing-read))
+ '(xref-show-definitions-function 'xref-show-definitions-completing-read t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
