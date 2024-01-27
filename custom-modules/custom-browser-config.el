45db7fab1430fd8d78518456e3ecbc72644e1d22
Jon Boone <ipmonger@delamancha.org>
2024-01-26 20:44:45 -0500

    update url-browse and xwidget-webkit config

--- custom-modules/custom-browser-config.el
+++ custom-modules/custom-browser-config.el
@@ -14,17 +14,35 @@
 (use-package browse-url
   :straight t
   :init
-  (customize-set-value browse-url-browser-function 'xwidget-webkit-browse-url))
+  ;; This function always opens a new website in a new window
+  (defun xwidget-browse-url-no-reuse (url &optional sessoin)
+    (interactive (progn
+                   (browse-url-interactive-arg "xwidget-webkit URL: ")))
+    (xwidget-webkit-browse-url url t))
+  (customize-set-value browse-url-browser-function
+                       (lambda (url session)
+                         (other-window 1)
+                         (xwidget-browse-url-no-reuse url))))
 
 
 (use-package xwwp-full
   :straight (xwwp-full :host github :repo "kchanqvq/xwwp")
+  :hooks
+  ;; adapt webkit according to window configuration change automatically
+  ;; without this hook, every time you change your window configuration,
+  ;; you must press 'a' to adapt webkit content to new window size
+  ('window-configuration-change-hook (lambda ()
+                                       (when (equal major-mode 'xwidget-webkit-mode)
+                                         (xwidget-webkit-adjust-size-dispatch))))
   :bind
   (:map xwidget-webkit-mode-map
-        ("<localleader>b" . 'xwidget-webkit-back)
-        ("<localleader>j" . 'xwwp-ace-toggle)
-        ("<localleader>o" . 'xwwp-section)
-        ("<localleader>h" . 'xwwp-history-show))
+        ("<localleader>b"      . 'xwidget-webkit-back)
+        ("<localleader>j"      . 'xwwp-ace-toggle)
+        ("<localleader>o"      . 'xwwp-section)
+        ("<localleader>h"      . 'xwwp-history-show)
+        ("<localleader><up>"   . 'xwidget-webkit-scroll-down)
+        ("<localleader><down>" . 'xwidget-webkit-scroll-up)
+        ((kbd "M-w")           . 'xwidget-webkit-copy-selection-as-kill))
   :init
   (customize-set-value xwwp-follow-link-completion-system 'default)
   :config
