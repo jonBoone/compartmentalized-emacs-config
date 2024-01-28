bdf9d325dde33cdd5d4ae6037607e402033403e2
Jon Boone <ipmonger@delamancha.org>
2024-01-27 21:06:57 -0500

    updating custom-browser-config settings

--- custom-modules/custom-browser-config.el
+++ custom-modules/custom-browser-config.el
@@ -19,6 +19,9 @@
     (interactive (progn
                    (browse-url-interactive-arg "xwidget-webkit URL: ")))
     (xwidget-webkit-browse-url url t))
+  (defun browse-url-adjust-size-dispatch ()
+    (when (equal major-mode 'xwidget-webkit-mode)
+      (xwidget-webkit-adjust-size-dispatch)))
   (customize-set-value browse-url-browser-function
                        (lambda (url session)
                          (other-window 1)
@@ -27,13 +30,11 @@
 
 (use-package xwwp-full
   :straight (xwwp-full :host github :repo "kchanqvq/xwwp")
-  :hooks
+  :hook
   ;; adapt webkit according to window configuration change automatically
   ;; without this hook, every time you change your window configuration,
   ;; you must press 'a' to adapt webkit content to new window size
-  ('window-configuration-change-hook (lambda ()
-                                       (when (equal major-mode 'xwidget-webkit-mode)
-                                         (xwidget-webkit-adjust-size-dispatch))))
+  (window-configuration-change-hook . browse-url-adjust-size-dispatch)
   :bind
   (:map xwidget-webkit-mode-map
         ("<localleader>b"      . 'xwidget-webkit-back)
@@ -42,11 +43,12 @@
         ("<localleader>h"      . 'xwwp-history-show)
         ("<localleader><up>"   . 'xwidget-webkit-scroll-down)
         ("<localleader><down>" . 'xwidget-webkit-scroll-up)
-        ((kbd "M-w")           . 'xwidget-webkit-copy-selection-as-kill))
+        ;; ((kbd "M-w")           . 'xwidget-webkit-copy-selection-as-kill)
+        )
   :init
-  (customize-set-value xwwp-follow-link-completion-system 'default)
   :config
-  (require 'cl-lib))
+  (require 'cl-lib)
+  (customize-set-value xwwp-follow-link-completion-system 'default))
 
 
 (provide 'custom-browser-config)
